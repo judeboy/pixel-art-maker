dfd3584b4d1b9da58cc9bfdb3e54282eddbdfa67
Judah Martin Trimmer <jutr4020@colorado.edu>
2017-10-17 11:53:15 -0600

    w/ papyrus background

--- pixel.js
+++ pixel.js
@@ -10,6 +10,7 @@ let grid = function () {
     column.style.width = '20px'
     column.style.float = 'left'
     column.className += 'pixels'
+    column.style['background-color'] = 'clear'
 
     for (let j = 0; j < 20; j++) {
       let rows = document.createElement('div');
@@ -26,8 +27,8 @@ let grid = function () {
   grid()
 
   let changeGridColor = function (event) {
-    event.target.style.backgroundColor = paintBrush;
-    return false;
+    event.target.style.backgroundColor = paintBrush
+    return false
   }
 
   for (let i = 0; i < document.getElementsByClassName('pixels').length; i++) {
@@ -38,37 +39,13 @@ let grid = function () {
 })
 
 function setPaletteEvent () {
-  let palette = document.getElementById('palette');
+  let palette = document.getElementById('palette')
   palette.addEventListener('click', changeColorOfPaintBrush)
 }
 
-let paintBrush = '#ff0000';
+let paintBrush = '#ff0000'
 
-function changeColorOfPaintBrush(event){paintBrush = event.target.style.backgroundColor;
+function changeColorOfPaintBrush(event){
+  paintBrush = event.target.style.backgroundColor
 }
-setPaletteEvent();
-
-
-
-//
-// })
-
-
-// let removeColor = function (event) {
-//   event.target.style.backgroundColor = 'white';
-//   return false;
-// }
-// let palette = function () {
-//   for (let y = 0; y < 4; y++) {
-//     let color = document.getElementById('con');
-//     let choice = document.createElement('div');
-//     color.appendChild(choice)
-//     choice.style['border-style'] = '1px'
-//     choice.style.backgroundColor = 'black'
-//     choice.style.width = '20px'
-//     choice.style.height = '20px'
-//     choice.style.float = 'left'
-//     choice.style.margin = '5px'
-//   }
-// }
-// palette()
+setPaletteEvent()
